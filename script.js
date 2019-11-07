3487b24f082eb2cb5002ec7ddd09544c4ad5977d
kmarusek <kmarusek91@gmail.com>
2019-11-06 21:05:05 -0500

    jslab1-finaledit

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
+
     const name = 'Mitch Cuckovich ';
-    const age = 25;
+    const age = 24;
     const birthday = 'January 24 ';
     const detroitGC = false;
     var counter = 0; 
+   
+    
+    
     let lifeEvents = ["I was born in Troy, Michigan. ", "I went to Hope College. ", "I went to the Junior Olympics when i was 10 years old. I placed 14th in the nation in the 800 meter event. ", "I'm a graduate of the front-end bootcamp. "];
 
     if(detroitGC == true){
@@ -16,4 +20,19 @@
     for (var i = 0; i < lifeEvents.length; i++){
         var item = lifeEvents[i];
         console.log(item);
+        
+    }
+    while (true){
+        let randomNumber = Math.floor(Math.random()
+         *10 + 1);
+         if (randomNumber !== 5){
+            counter++;
+            console.log(randomNumber+" !==5");
+    }
+
+        else {
+            counter++;
+            console.log(randomNumber+" === 5"+" it took "+counter+" iterations to randomly generate the number 5.");
+            break;      
+        }
     }
